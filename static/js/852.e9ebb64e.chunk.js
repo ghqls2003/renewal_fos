1f3db6e3939dea88ff46c3cdb68270b3cb73afc0
Ryong <kryong@neighbor21.co.kr>
2023-07-06 14:40:23 +0900

    Updates

---
+++ static/js/852.e9ebb64e.chunk.js
@@ -0,0 +1,29 @@
+"use strict";(self.webpackChunkberry_free_material_react_cra=self.webpackChunkberry_free_material_react_cra||[]).push([[852],{8852:function(o,t,c){c.r(t),c.d(t,{default:function(){return j}});var e=c(9019),s=c(3497),p=(c(7313),c(346)),a=c(3122),i=c(6417);const n=p.ZP.span`
+  margin-right: 10px;
+  cursor: pointer;
+  &:hover {
+    .icons {
+      padding: 5px;
+      color: white;
+      background-color: black;
+    }
+  }
+`;var r=function(o){let{view:t}=o;return"one"===t?(0,i.jsx)(n,{children:(0,i.jsx)(a.JuG,{className:"icons",size:30})}):"two"===t?(0,i.jsx)(n,{children:(0,i.jsx)(a.xyo,{className:"icons",size:30})}):"three"===t?(0,i.jsx)(n,{children:(0,i.jsx)(a.uOL,{className:"icons",size:30})}):void 0};const h=p.ZP.div`
+  padding-bottom: 6%;
+  column-count: 4; // 사진 가로 갯수
+  column-gap: 1em;
+`,d=p.ZP.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 1em;
+  cursor: pointer;
+`,l=p.ZP.div`
+  display: inline-block;
+  filter: grayscale(0.8);
+
+  &:hover {
+    filter: none;
+  }
+`,x=p.ZP.img`
+  width: 100%;
+`;var _=()=>(0,i.jsxs)("div",{children:[(0,i.jsx)(r,{view:"one"}),(0,i.jsx)(r,{view:"two"}),(0,i.jsx)(r,{view:"three"}),(0,i.jsx)(h,{children:["https://cdn.pixabay.com/photo/2020/09/02/20/52/dock-5539524__340.jpg","https://cdn.pixabay.com/photo/2021/02/03/13/54/cupcake-5978060__340.jpg","https://cdn.pixabay.com/photo/2020/05/25/20/14/holland-iris-5220407__340.jpg","https://cdn.pixabay.com/photo/2020/10/08/17/39/waves-5638587__340.jpg","https://cdn.pixabay.com/photo/2019/01/30/11/17/zebra-3964360__340.jpg","https://cdn.pixabay.com/photo/2021/02/01/13/37/cars-5970663__340.png","https://cdn.pixabay.com/photo/2019/06/05/10/34/mimosa-4253396__340.jpg","https://cdn.pixabay.com/photo/2020/08/04/14/42/sky-5463015__340.jpg","https://cdn.pixabay.com/photo/2021/02/03/13/54/cupcake-5978060__340.jpg","https://cdn.pixabay.com/photo/2020/01/09/01/00/the-eye-on-the-greek-4751572__340.png","https://cdn.pixabay.com/photo/2021/01/30/12/19/couple-5963678__340.png","https://cdn.pixabay.com/photo/2021/01/23/07/53/dogs-5941898__340.jpg","https://cdn.pixabay.com/photo/2020/06/15/01/06/sunset-5299957__340.jpg"].map(((o,t)=>(0,i.jsx)(d,{children:(0,i.jsx)(l,{children:(0,i.jsx)(x,{src:o})})},t)))})]}),m=c(2401);var j=()=>(0,i.jsx)(s.Z,{title:"2010~2016 \uc0ac\uc9c4\ub4e4",children:(0,i.jsx)(e.ZP,{container:!0,spacing:m.dv,children:(0,i.jsx)(e.ZP,{item:!0,xs:12,sm:12,children:(0,i.jsx)(_,{})})})})}}]);
